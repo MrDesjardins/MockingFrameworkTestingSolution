212933707bab17738f5a8e49a0f008b06205169e
MrDesjardins <mrdesjardins@gmail.com>
2013-12-01 13:48:44 -0500

    First commit

---
+++ BusinessLogic/StockSimulator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class Stock
+    {
+        public string Symbol { get; set; }
+        public double Price { get; set; }
+    }
+
+    public class ExceptionStockNotFound : Exception{}
+
+    public interface IStockPriceManager
+    {
+        double GetCurrentPrice(Stock stock);
+    }
+
+    public class StockPriceManager : IStockPriceManager
+    {
+        public double GetCurrentPrice(Stock stock)
+        {
+            Thread.Sleep(5000);//Simulate internet time
+            if (stock.Symbol == "MSFT")
+                return 35.15;
+            if (stock.Symbol == "GOOG")
+                return 1001.87;
+            throw new ExceptionStockNotFound();
+        }
+    }
+
+    public class StockSimulator
+    {
+        public IStockPriceManager StockPriceManager { get; set; }
+
+        public StockSimulator(IStockPriceManager stockPriceManager)
+        {
+            StockPriceManager = stockPriceManager;
+        }
+
+        public bool IsStockGood(Stock stock)
+        {
+            stock.Price = StockPriceManager.GetCurrentPrice(stock);
+            return stock.Price > 0;
+        }
+    }
+
+    public class FakeStockPriceManager:IStockPriceManager
+    {
+        public double GetCurrentPrice(Stock stock)
+        {
+            return 200;
+        }
+    }
+    
+}
---
+++ Tests/StockSimulatorTest.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using BusinessLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NSubstitute;
+using NMock;
+using Rhino.Mocks;
+using MockFactory = NMock.MockFactory;
+
+namespace Tests
+{
+    [TestClass]
+    public class StockSimulatorTest
+    {
+        private Stock stock;
+
+        [TestInitialize]
+        public void InitializeBetweenTest()
+        {
+            this.stock = new Stock { Symbol = "MSFT" };
+        }
+
+        [TestMethod]
+        public void WhenStockPriceAtZero_ExpectNotBuy()
+        {
+            //Arrange
+            #region moq
+            //var mock = new Mock<IStockPriceManager>();
+            //mock.Setup(foo => foo.GetCurrentPrice(stock)).Returns(0);
+            //var stockSimulator = new StockSimulator(mock.Object);
+            #endregion
+            #region NSubstitute
+            //var mock = Substitute.For<IStockPriceManager>();
+            //mock.GetCurrentPrice(stock).Returns(0);
+            //var stockSimulator = new StockSimulator(mock);
+            #endregion
+            #region nMock3
+            //var mockFactory = new MockFactory();
+            //var mock = mockFactory.CreateMock<IStockPriceManager>();
+            //mock.Expects.One.Method(d => d.GetCurrentPrice(stock)).With(stock).WillReturn(0);
+            //var stockSimulator = new StockSimulator(mock.MockObject);
+            #endregion
+            #region RhinoMock
+            var mock = Rhino.Mocks.MockRepository.GenerateMock<IStockPriceManager>();
+            mock.Expect(d => d.GetCurrentPrice(stock)).Return(0);
+            var stockSimulator = new StockSimulator(mock);
+            #endregion
+            //Act
+            var answer = stockSimulator.IsStockGood(stock);
+
+            //Assert
+            Assert.IsFalse(answer);
+        }
+
+        [TestMethod]
+        public void WhenStockPriceOverZero_ExpectBuy()
+        {
+            //Arrange
+            #region moq
+            //var mock = new Mock<IStockPriceManager>();
+            //mock.Setup(foo => foo.GetCurrentPrice(stock)).Returns(1);
+            //var stockSimulator = new StockSimulator(mock.Object);
+            #endregion
+            #region NSubstitute
+            //var mock = Substitute.For<IStockPriceManager>();
+            //mock.GetCurrentPrice(stock).Returns(1);
+            //var stockSimulator = new StockSimulator(mock);
+            #endregion
+            #region nMock3
+            //var mockFactory = new MockFactory();
+            //var mock = mockFactory.CreateMock<IStockPriceManager>();
+            //mock.Expects.One.Method(d => d.GetCurrentPrice(stock)).With(stock).WillReturn(1);
+            //var stockSimulator = new StockSimulator(mock.MockObject);
+            #endregion
+            #region RhinoMock
+            var mock = Rhino.Mocks.MockRepository.GenerateMock<IStockPriceManager>();
+            mock.Expect(d => d.GetCurrentPrice(stock)).Return(1);
+            var stockSimulator = new StockSimulator(mock);
+            #endregion
+            //Act
+            var answer = stockSimulator.IsStockGood(stock);
+
+            //Assert
+            Assert.IsTrue(answer);
+        }
+
+
+        [TestMethod]
+        public void WhenStockPriceUnderZero_ExpectBuy()
+        {
+            //Arrange
+            #region moq
+            //var mock = new Mock<IStockPriceManager>();
+            //mock.Setup(foo => foo.GetCurrentPrice(stock)).Returns(-1);
+            //var stockSimulator = new StockSimulator(mock.Object);
+            #endregion
+            #region NSubstitute
+            //var mock = Substitute.For<IStockPriceManager>();
+            //mock.GetCurrentPrice(stock).Returns(-1);
+            //var stockSimulator = new StockSimulator(mock);
+            #endregion
+            #region nMock3
+            //var mockFactory = new MockFactory();
+            //var mock = mockFactory.CreateMock<IStockPriceManager>();
+            //mock.Expects.One.Method(d => d.GetCurrentPrice(stock)).With(stock).WillReturn(-1);
+            //var stockSimulator = new StockSimulator(mock.MockObject);
+            #endregion
+            #region RhinoMock
+            var mock = Rhino.Mocks.MockRepository.GenerateMock<IStockPriceManager>();
+            mock.Expect(d => d.GetCurrentPrice(stock)).Return(-1);
+            var stockSimulator = new StockSimulator(mock);
+            #endregion
+            //Act
+            var answer = stockSimulator.IsStockGood(stock);
+
+            //Assert
+            Assert.IsFalse(answer);
+        }
+
+
+        [TestMethod]
+        public void WhenStockNotFound_ExpectException()
+        {
+            //Arrange
+            #region moq
+            //var mock = new Mock<IStockPriceManager>();
+            //mock.Setup(foo => foo.GetCurrentPrice(stock)).Throws<ExceptionStockNotFound>();
+            //var stockSimulator = new StockSimulator(mock.Object);
+            #endregion
+            #region NSubstitute
+            //var mock = Substitute.For<IStockPriceManager>();
+            //mock.GetCurrentPrice(stock).Returns(x => { throw new ExceptionStockNotFound(); });
+            //var stockSimulator = new StockSimulator(mock);
+            #endregion
+            #region nMock3
+            //var mockFactory = new MockFactory();
+            //var mock = mockFactory.CreateMock<IStockPriceManager>();
+            //mock.Expects.One.Method(d => d.GetCurrentPrice(stock)).With(stock).Will(Throw.Exception(new ExceptionStockNotFound()));
+            //var stockSimulator = new StockSimulator(mock.MockObject);
+            #endregion
+            #region RhinoMock
+            var mock = Rhino.Mocks.MockRepository.GenerateMock<IStockPriceManager>();
+            mock.Expect(d => d.GetCurrentPrice(stock)).Throw(new ExceptionStockNotFound());
+            var stockSimulator = new StockSimulator(mock);
+            #endregion
+            //Act & Assert
+            try
+            {
+                stockSimulator.IsStockGood(stock);
+                Assert.Fail("Should never go here");
+            }
+            catch (ExceptionStockNotFound e)
+            {
+                //This is what we want
+            }
+           
+        }
+    }
+}
